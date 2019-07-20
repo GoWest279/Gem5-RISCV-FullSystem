3a313e41a702a803e6a01c902e50c8952078b024
john-liu2039 <jl@ad2039.com>
2019-07-19 21:26:58 -0700

    update registers.hh
    Change-Id: I9467c95201e527ed906b0e59d6cb62049cc57aab

--- src/arch/riscv/registers.hh
+++ src/arch/riscv/registers.hh
@@ -92,7 +92,7 @@ const int NumFloatRegs = 32;
 const unsigned NumVecRegs = 1;  // Not applicable to RISC-V
                                 // (1 to prevent warnings)
 const int NumVecPredRegs = 1;  // Not applicable to RISC-V
-                               // (1 to prevent warnings)
+                                // (1 to prevent warnings)
 
 const int NumCCRegs = 0;
 
@@ -113,24 +113,24 @@ const std::vector<int> SyscallArgumentRegs = {10, 11, 12, 13, 14, 15, 16};
 const int SyscallNumReg = 17;
 
 const std::vector<std::string> IntRegNames = {
-    "zero", "ra", "sp", "gp",
-    "tp", "t0", "t1", "t2",
-    "s0", "s1", "a0", "a1",
-    "a2", "a3", "a4", "a5",
-    "a6", "a7", "s2", "s3",
-    "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11",
-    "t3", "t4", "t5", "t6"
+        "zero", "ra", "sp", "gp",
+        "tp", "t0", "t1", "t2",
+        "s0", "s1", "a0", "a1",
+        "a2", "a3", "a4", "a5",
+        "a6", "a7", "s2", "s3",
+        "s4", "s5", "s6", "s7",
+        "s8", "s9", "s10", "s11",
+        "t3", "t4", "t5", "t6"
 };
 const std::vector<std::string> FloatRegNames = {
-    "ft0", "ft1", "ft2", "ft3",
-    "ft4", "ft5", "ft6", "ft7",
-    "fs0", "fs1", "fa0", "fa1",
-    "fa2", "fa3", "fa4", "fa5",
-    "fa6", "fa7", "fs2", "fs3",
-    "fs4", "fs5", "fs6", "fs7",
-    "fs8", "fs9", "fs10", "fs11",
-    "ft8", "ft9", "ft10", "ft11"
+        "ft0", "ft1", "ft2", "ft3",
+        "ft4", "ft5", "ft6", "ft7",
+        "fs0", "fs1", "fa0", "fa1",
+        "fa2", "fa3", "fa4", "fa5",
+        "fa6", "fa7", "fs2", "fs3",
+        "fs4", "fs5", "fs6", "fs7",
+        "fs8", "fs9", "fs10", "fs11",
+        "ft8", "ft9", "ft10", "ft11"
 };
 
 enum MiscRegIndex {
@@ -215,12 +215,12 @@ enum MiscRegIndex {
     MISCREG_MEDELEG,
     MISCREG_MIDELEG,
     MISCREG_MTVEC,
-    MISCREG_MIE,
     MISCREG_MCOUNTEREN,
     MISCREG_MSCRATCH,
     MISCREG_MEPC,
     MISCREG_MCAUSE,
     MISCREG_MTVAL,
+    MISCREG_MIE,
     MISCREG_PMPCFG0,
     // pmpcfg1 rv32 only
     MISCREG_PMPCFG2,
